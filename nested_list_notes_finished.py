53af9d85e3503d55fb31c3216efab953fc80415e
Matt Callaghan <mcallaghan@naperville203.org>
2021-12-06 07:42:19 -0600

    Update nested_list_notes_finished.py

--- nested_list_notes_finished.py
+++ nested_list_notes_finished.py
@@ -1,11 +1,13 @@
 """
-  nested lists notes 
-  this program continues to use the idea of a row of cars as a list
-
-  What are nested lists?
-    A nested list is a two-dimensional list, or a list containing 
-      other lists or sequences. Think of like a matrix with rows and columns,
-      or in the case of a row of cars, a parking lot.
+  Name: 
+  Date:
+	Title: 2D lists and nested loops notes 
+	Description: Introduces Two-dimensional lists (lists within lists) and nested for loops. This program continues to use the idea of a row of cars to represent a list.
+"""
+"""
+  What are 2D lists or nested lists?
+    A 2D list is a two-dimensional list, or a list containing 
+      other lists or sequences. Think of it like a matrix with rows and columns, or in the case of a row of cars, a parking lot of rows and spaces.
       e.g.:
       
             col1    col2    col3    col4
@@ -16,7 +18,7 @@
     
     The more general term is a nested sequence (applies to tuples as well).
 """
-#1 Creating a nested list (our parking lot)
+#1 Creating a 2D list (i.e. our parking lot of cars)
 lot = [
     ["ford", "toyota", "buick", "gm"],
     ["nissan", "lexus", "jeep", "ford"],
@@ -29,13 +31,13 @@ if "ford" in lot:
 else:
     print (False)
 
-#2 Accessing nested elements (rows of cars)
+#2 Accessing a nested list (i.e. a row of cars)
 print (lot[0])
 print (lot[1])
 print (lot[2])
 print (lot[3])
 
-#3 Accessing an individual element within a nested element
+#3 Accessing an individual element from a 2D list (i.e. a car)
 print (lot[0][1])
 
 # On your own: display each of the following...
@@ -68,9 +70,8 @@ print(lot[0][0])
 
 input("\nPress Enter to Continue...")
 
-# Challenge!
-print("\nTry printing the lot in column format with a single tab in between each car. \n \
-Also, display row and column headings accordingly, without hardcoding:")
+# Using nested for loops to display the individual elements in row / column format
+print("\nPrint the lot in row / column format with a single tab in between each car. \nAlso, display row and column headings accordingly, without hardcoding:")
 for col in range(len(lot[0])):
     print("\tcolumn", col+1, end="")
 print()
@@ -82,9 +83,8 @@ for row in range(len(lot)):
 
 input("\nPress Enter to Continue...")
 
-#4 Unpacking a sequence
-#  Unpacking a sequence means assigning each element
-#   its own variable in a single line of code.
+#4 Unpacking a sequence - assigning each element of a sequence its own variable in a single line of code.
+
 
 # Another nested list example: 
 a_row = [["Ford", "Fusion"], ["Audi", "A4"], ["BMW", "3 Series"],["Jeep", "Wrangler"]] 
@@ -102,12 +102,12 @@ spot = [make, model]
 a_row.append(spot)
 print(a_row) 
 
-#6 Sorting a nested sequence
+#6 Sorting a 2D list
 print("Row of cars sorted by make:")
 a_row.sort()
 print(a_row) 
 
-#7 Sorting a nested sequence by elements other than the first
+#7 Sorting a 2D list by elements other than the first
 #  We have a new module that we need to use... operator (and the itemgetter function).
 print("Row of cars sorted by model:")
 
